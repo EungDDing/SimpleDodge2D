04f45a1b5ef7dce65c8d9ccb3ccfc3686f7eeaca
이응민 <ppoppo9802@naver.com>
2025-02-20 02:59:32 +0900

    Update 0220

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject gameOver;
+    [SerializeField] private TextMeshProUGUI timerText;
+    private float time;
 
     private void Awake()
     {
+        time = 0;
         gameOver.SetActive(false);
         Meteoroid.GameOver += OpenGameOver;
+    }
+    private void Update()
+    {
+        time += Time.deltaTime;
+        timerText.text = string.Format("{0}", (int)time);
     }
 
     public void OpenGameOver()
